b8f18a1da0bf06f0eb8f189bba4eee0c1435f53b
Daniel Jordi Perez <danipq26@gmail.com>
2017-12-19 19:12:57 +0100

    fixed deprecation

--- src/test/java/es/rachelcarmena/couplers/PaymentShould.java
+++ src/test/java/es/rachelcarmena/couplers/PaymentShould.java
@@ -4,6 +4,7 @@ import es.rachelcarmena.couplers.smell1.Payment;
 import org.junit.Test;
 
 import java.math.BigDecimal;
+import java.math.RoundingMode;
 import java.time.LocalDate;
 
 import static org.hamcrest.core.Is.is;
@@ -16,7 +17,7 @@ public class PaymentShould {
     public void allow_get_amount_with_2_decimals() {
         Payment payment = new Payment("000012", "12.4563", "OneMethod", LocalDate.now());
         BigDecimal paymentValue = payment.getAmountValue();
-        String printedValue = paymentValue.setScale(2, BigDecimal.ROUND_HALF_EVEN).toString();
+        String printedValue = paymentValue.setScale(2, RoundingMode.HALF_EVEN).toString();
 
         assertThat(printedValue, is("12.46"));
     }
@@ -25,7 +26,7 @@ public class PaymentShould {
     public void allow_get_amount_with_2_decimals_when_no_decimals() {
         Payment payment = new Payment("000012", "18", "OneMethod", LocalDate.now());
         BigDecimal paymentValue = payment.getAmountValue();
-        String printedValue = paymentValue.setScale(2, BigDecimal.ROUND_HALF_EVEN).toString();
+        String printedValue = paymentValue.setScale(2, RoundingMode.HALF_EVEN).toString();
 
         assertThat(printedValue, is("18.00"));
     }
