a28db329f6c2ea81fbac4dd57c6543890bf288e2
Daniel Jordi Perez <danipq26@gmail.com>
2018-01-03 18:09:58 +0100

    refactor: Refused Bequest smell, replaced inheritance with delegation

--- src/main/java/es/rachelcarmena/oo/abusers/smell1/CoolStack.java
+++ src/main/java/es/rachelcarmena/oo/abusers/smell1/CoolStack.java
@@ -2,15 +2,24 @@ package es.rachelcarmena.oo.abusers.smell1;
 
 import java.util.Vector;
 
-public class CoolStack extends Vector {
+public class CoolStack {
+    private Vector<Object> stack = new Vector<>();
 
     public void push(Object element) {
-        insertElementAt(element, 0);
+        stack.insertElementAt(element, 0);
     }
 
     public Object pop() {
-        Object result = firstElement();
-        removeElementAt(0);
+        Object result = stack.firstElement();
+        stack.removeElementAt(0);
         return result;
+    }
+
+    public Integer size() {
+        return this.stack.size();
+    }
+
+    public boolean isEmpty() {
+        return this.stack.isEmpty();
     }
 }
